9815635ad6139f24735a47740b21ebc1c829e80a
Maciek Skokowski <maciejskok2004@gmail.com>
2024-01-19 15:19:37 +0100

    added functionality of equal btn and added modulo

--- src/components/CalcBtnsContainer.jsx
+++ src/components/CalcBtnsContainer.jsx
@@ -1,3 +1,109 @@
+import { useState, useEffect } from 'react';
+
+export function MainCalc() {
+	const [inputValue, setInputValue] = useState('');
+
+	const handleBtnFunction = btnValue => {
+		switch (btnValue) {
+			case 'del':
+				setInputValue(prevState => prevState.slice(0, -1));
+				break;
+			case 'reset':
+				setInputValue('');
+				break;
+			case '=':
+				evalResults();
+				break;
+			default:
+				setInputValue(prevState => prevState + btnValue);
+				break;
+		}
+	};
+
+	const evalResults = () => {
+		try {
+			// const decimalPart = inputValue.split('.')[1];
+			const result = eval(inputValue);
+
+			if (Number.isFinite(result)) {
+				setInputValue(prevState => eval(prevState).toFixed(2))
+			}
+
+		} catch (error) {
+			console.error(error);
+			setInputValue('');
+		}
+	};
+
+	useEffect(() => {
+		const inputEl = document.querySelector('.display-calc-input');
+
+		inputEl.value = inputValue;
+	});
+
+	return (
+		<>
+			<button className='calc-btn greens' onClick={() => handleBtnFunction('del')}>
+				←
+			</button>
+			<button className='calc-btn greens' onClick={() => handleBtnFunction('reset')}>
+				C
+			</button>
+			<button className='calc-btn reds' onClick={() => handleBtnFunction('/')}>
+				/
+			</button>
+			<button className='calc-btn reds' onClick={() => handleBtnFunction('*')}>
+				*
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('7')}>
+				7
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('8')}>
+				8
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('9')}>
+				9
+			</button>
+			<button className='calc-btn reds' onClick={() => handleBtnFunction('-')}>
+				-
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('4')}>
+				4
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('5')}>
+				5
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('6')}>
+				6
+			</button>
+			<button className='calc-btn reds' onClick={() => handleBtnFunction('+')}>
+				+
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('1')}>
+				1
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('2')}>
+				2
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('3')}>
+				3
+			</button>
+			<button className='calc-btn reds' onClick={() => handleBtnFunction('%')}>
+				%
+			</button>
+			<button className='calc-btn' data-value="0" onClick={() => handleBtnFunction('0')}>
+				0
+			</button>
+			<button className='calc-btn' onClick={() => handleBtnFunction('.')}>
+				.
+			</button>
+			<button className='calc-btn reds' onClick={() => handleBtnFunction('=')}>
+				=
+			</button>
+		</>
+	);
+}
+
 export function DisplayCalc() {
 	return (
 		<>
@@ -15,78 +121,6 @@ export function DisplayCalc() {
 				//   !allowedKeys.includes(e.key) ? e.preventDefault() : '';
 				// }}
 			/>
-		</>
-	);
-}
-
-export function MainCalc() {
-	return (
-		<>
-			<section className='calc-btns-section-x'>
-				<button className='calc-btn special' data-value='del'>
-					←
-				</button>
-				<button className='calc-btn special' data-value='reset'>
-					C
-				</button>
-				<button className='calc-btn special' data-value='/'>
-					/
-				</button>
-				<button className='calc-btn special' data-value='*'>
-					x
-				</button>
-			</section>
-			<section className='calc-btns-section-x'>
-				<button className='calc-btn' data-value='7'>
-					7
-				</button>
-				<button className='calc-btn' data-value='8'>
-					8
-				</button>
-				<button className='calc-btn' data-value='9'>
-					9
-				</button>
-				<button className='calc-btn special' data-value='-'>
-					-
-				</button>
-			</section>
-			<section className='calc-btns-section-x'>
-				<button className='calc-btn' data-value='4'>
-					4
-				</button>
-				<button className='calc-btn' data-value='5'>
-					5
-				</button>
-				<button className='calc-btn' data-value='6'>
-					6
-				</button>
-				<button className='calc-btn special' data-value='+'>
-					+
-				</button>
-			</section>
-			<section className='calc-btns-section-x'>
-				<button className='calc-btn' data-value='1'>
-					1
-				</button>
-				<button className='calc-btn' data-value='2'>
-					2
-				</button>
-				<button className='calc-btn' data-value='3'>
-					3
-				</button>
-				<button className='calc-btn special' data-value='='>
-					=
-				</button>
-			</section>
-			<section className='calc-btns-section-x'>
-				<button className='calc-btn' data-value='0'>
-					0
-				</button>
-				<button className='calc-btn' data-value='.'>
-					.
-				</button>
-				<button className='calc-btn'></button>
-			</section>
 		</>
 	);
 }
